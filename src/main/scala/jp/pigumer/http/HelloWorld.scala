1ab336a92dcb581c1bf629257bf351a4c1a0e479
Shigeki Shoji <takesection@gmail.com>
2018-05-02 11:14:07 +0900

    add exception handling

--- src/main/scala/jp/pigumer/http/HelloWorld.scala
+++ src/main/scala/jp/pigumer/http/HelloWorld.scala
@@ -2,15 +2,14 @@ package jp.pigumer.http
 
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshalling.ToResponseMarshallable
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.{RequestContext, StandardRoute}
+import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Flow, Sink, Source}
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 class Server {
 
@@ -26,8 +25,15 @@ class Server {
       get {
         complete {
           val source: Source[String, NotUsed] = Source.single(message)
-          val helloWorld: Flow[String, ToResponseMarshallable, NotUsed] = Flow[String].map { _ ⇒
-            HttpEntity(ContentTypes.`text/html(UTF-8)`, s"<h1>${message}</h1>")
+          val helloWorld: Flow[String, ToResponseMarshallable, NotUsed] = Flow[String].map {
+            case "error" ⇒
+              HttpResponse(StatusCodes.InternalServerError)
+            case "notfound" ⇒
+              HttpResponse(StatusCodes.NotFound)
+            case "NPE" ⇒
+              throw new NullPointerException()
+            case s ⇒
+              HttpEntity(ContentTypes.`text/html(UTF-8)`, s"<h1>${s}</h1>")
           }
           val sink: Sink[ToResponseMarshallable, Future[ToResponseMarshallable]] = Sink.head[ToResponseMarshallable]
           for (f <- source.via(helloWorld).runWith(sink)) yield f
@@ -45,7 +51,7 @@ object HelloWorld extends App {
 
   sys.addShutdownHook {
     val system = server.system
-    implicit val executionContext = server.executionContext
+    implicit val executionContext: ExecutionContext = server.executionContext
     bindingFuture
       .flatMap(_.unbind()) // trigger unbinding from the port
       .onComplete(_ => system.terminate()) // and shutdown when done
