f075668949491c595fe6a55415c2d91e8057c967
Shigeki Shoji <takesection@gmail.com>
2018-05-06 20:31:25 +0900

    edit

--- src/main/scala/jp/pigumer/deadletter/DeadLetterMonitor.scala
+++ src/main/scala/jp/pigumer/deadletter/DeadLetterMonitor.scala
@@ -1,12 +1,20 @@
 package jp.pigumer.deadletter
 
-import akka.actor.{Actor, DeadLetter}
+import akka.actor.{Actor, ActorRef, DeadLetter}
 import akka.event.Logging
+import akka.stream._
+import akka.stream.scaladsl.{Flow, Sink, Source}
 
 class DeadLetterMonitor extends Actor {
   private val logger = Logging(context.system, this)
+  private implicit val materializer = ActorMaterializer()(context)
+  private val src: Source[DeadLetter, ActorRef] = Source.actorRef[DeadLetter](100, OverflowStrategy.fail)
+  private val flow: ActorRef = Flow[DeadLetter].to(Sink.foreach {
+    case DeadLetter(message, sender, recipient) ⇒
+      logger.info(s"deadletter $recipient $message")
+  }).runWith(src)
   override def receive: Receive = {
-    case DeadLetter(msg, sender, recipient) ⇒
-      logger.info(s"deadletter $recipient $msg")
+    case deadLetter ⇒
+      flow ! deadLetter
   }
 }
---
+++ src/main/scala/jp/pigumer/deadletter/DeadLetterPublisher.scala
@@ -0,0 +1,7 @@
+package jp.pigumer.deadletter
+
+import akka.actor.Actor
+
+class DeadLetterPublisher extends Actor {
+  override def receive: Receive = ???
+}
