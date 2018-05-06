c94740bffae4749c4643383c0159018b3bdaaf47
Shigeki Shoji <takesection@gmail.com>
2018-05-06 23:44:15 +0900

    edit

--- src/main/scala/jp/pigumer/deadletter/DeadLetterMonitor.scala
+++ src/main/scala/jp/pigumer/deadletter/DeadLetterMonitor.scala
@@ -1,20 +1,27 @@
 package jp.pigumer.deadletter
 
-import akka.actor.{Actor, ActorRef, DeadLetter}
+import akka.Done
+import akka.actor.{Actor, DeadLetter}
 import akka.event.Logging
 import akka.stream._
-import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.stream.scaladsl.{Keep, Sink, Source, SourceQueueWithComplete}
+
+import scala.concurrent.Future
 
 class DeadLetterMonitor extends Actor {
   private val logger = Logging(context.system, this)
   private implicit val materializer = ActorMaterializer()(context)
-  private val src: Source[DeadLetter, ActorRef] = Source.actorRef[DeadLetter](100, OverflowStrategy.fail)
-  private val flow: ActorRef = Flow[DeadLetter].to(Sink.foreach {
+
+  private val src: Source[DeadLetter, SourceQueueWithComplete[DeadLetter]] =
+    Source.queue[DeadLetter](100, OverflowStrategy.backpressure)
+  private val sink: Sink[DeadLetter, Future[Done]] = Sink.foreach {
     case DeadLetter(message, sender, recipient) ⇒
-      logger.info(s"deadletter $recipient $message")
-  }).runWith(src)
+      logger.info(s"$src $recipient $message")
+  }
+  private val (queue, _) = src.toMat(sink)(Keep.both).run()
+
   override def receive: Receive = {
-    case deadLetter ⇒
-      flow ! deadLetter
+    case deadLetter: DeadLetter ⇒
+      queue.offer(deadLetter)
   }
 }
